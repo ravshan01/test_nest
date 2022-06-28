1ed3d33eea4451c85f67b1aba143663c02c726dd
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-06-28 18:00:12 +0500

    base realized user.entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  login: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  country: string;
+}

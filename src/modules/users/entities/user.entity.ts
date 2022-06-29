79299f5aafcc26d9a67dba4d3a8a5da801864264
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-06-29 15:50:28 +0500

    update user entity
    add created_at, deleted_at fields

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -16,4 +22,10 @@ export class User {
 
   @Column()
   country: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
 }

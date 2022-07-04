96a65996048bb36bb0154f7bb98ab65eb0777d25
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-04 18:26:42 +0500

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './modules/core/core.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -17,7 +19,9 @@ import { UsersModule } from './modules/users/users.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    CoreModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],

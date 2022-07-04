8da3e06ebff15e3830b1ff4ee0dacdba3588907b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-04 18:27:09 +0500

    create auth guard

---
+++ src/modules/auth/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    try {
+      const token = request.headers.authorization.split(' ')[1];
+      const data = this.jwtService.verify(token);
+      if (!data) return false;
+    } catch (err) {
+      switch (err.name) {
+        case 'TokenExpiredError': {
+          throw new UnauthorizedException();
+        }
+        case 'JsonWebTokenError': {
+          throw new UnauthorizedException();
+        }
+        case 'NotBeforeError': {
+          throw new UnauthorizedException();
+        }
+        default: {
+          throw new UnauthorizedException();
+        }
+      }
+    }
+
+    return true;
+  }
+}

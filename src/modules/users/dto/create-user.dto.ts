71434baa8cd0bfa697ca6b9c91ca180849611f31
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-10 18:10:52 +0500

    update create-user.dto.ts

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  public login: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+
+  @IsString()
+  @IsOptional()
+  public country: string;
+}
